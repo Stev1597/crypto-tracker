12136803dac1e9b0b7f49264275bde35bc84eb9a
Stev1597 <stevendabescat15@gmail.com>
2025-10-15 17:38:31 +0200

    Update alertes_tokens.py

--- alertes_tokens.py
+++ alertes_tokens.py
@@ -76,7 +76,7 @@ def detecter_scenarios(token, premier_prix, est_suivi):
     alerts = []
     name = token.get("nom_jeton") or "Token"
     address = token.get("token_address")
-    lien = f"https://dexscreener.com/solana/{address}"
+    lien = f"https://app.axiom.trade/token/{address}?chain=solana"
     prix_actuel = token.get("price")
     mcap = token.get("marketcap")
     debut = datetime.fromisoformat(token["created_at"].replace("Z", "+00:00"))
