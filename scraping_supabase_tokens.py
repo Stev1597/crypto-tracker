388bffe9681a7931b407400a1d22d8cf4c0fdde2
Stev1597 <stevendabescat15@gmail.com>
2025-10-21 21:29:03 +0200

    Update scraping_supabase_tokens.py

--- scraping_supabase_tokens.py
+++ scraping_supabase_tokens.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 from supabase import create_client, Client
 
 # ------------------ CONFIG ------------------ #
-API_KEY = os.getenv("MORALIS_API_KEY")
+API_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImQ1YzYwZjJjLWYwOTUtNDQyNS04NWZlLTU5N2FjNDJhYjBhMiIsIm9yZ0lkIjoiNDU4NzczIiwidXNlcklkIjoiNDcxOTk3IiwidHlwZUlkIjoiNjVlMjllNmEtZWRlOS00ZGRlLWIzMGQtOGExODA2YjJmZGFjIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NTIyMjMwODgsImV4cCI6NDkwNzk4MzA4OH0.qDcce8fW8tjaRQ5RupNun0HHasfMAG31i5fEHTVtcZo")
 SUPABASE_URL = "https://mwnejkrkjlnrwrulqedd.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13bmVqa3JramxucndydWxxZWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4OTc4NzYsImV4cCI6MjA2OTQ3Mzg3Nn0.6gCD-zi1nFK4m61bLBzYKmuE48ZqKOgVclelebO9vUk"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
