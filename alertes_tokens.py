ae7497a6f3d235ddc3197062b23ccd13104ec8c4
Stev1597 <stevendabescat15@gmail.com>
2025-11-13 01:04:10 +0100

    Update alertes_tokens.py

--- alertes_tokens.py
+++ alertes_tokens.py
@@ -382,7 +382,7 @@ def detecter_migration_top10():
 
 
 # ▶️ MAIN
-def vérifier_alertes():
+def verifier_alertes():
     print(f"\n[🔔 CYCLE ALERTES] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     mettre_a_jour_date_suivi()  # Met à jour la date_suivi si manquante
     try:
